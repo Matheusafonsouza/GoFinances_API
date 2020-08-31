8c3432e311a0fb5fa904b5d4bc536326feea7a8a
Matheus Afonso <matheusafonsouza@gmail.com>
2020-08-31 18:12:11 -0300

    feature: Created import method for transactions

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,52 @@
+import csvParse from 'csv-parse';
+import fs from 'fs';
+import path from 'path';
 import Transaction from '../models/Transaction';
+import CreateTransactionService from './CreateTransactionService';
+
+interface TransactionCSV {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(filename: string): Promise<Transaction[]> {
+    const csvTransactions: TransactionCSV[] = [];
+
+    const transactionRepository = new CreateTransactionService();
+
+    const csvFilePath = path.resolve(__dirname, '..', '..', 'tmp', filename);
+
+    const readCSVStream = fs.createReadStream(csvFilePath);
+
+    const parseStream = csvParse({
+      from_line: 2,
+      ltrim: true,
+      rtrim: true,
+    });
+
+    const parseCSV = readCSVStream.pipe(parseStream);
+
+    await parseCSV.on('data', line => {
+      const [title, type, value, category] = line;
+
+      csvTransactions.push({ title, type, value, category });
+    });
+
+    await new Promise(resolve => {
+      parseCSV.on('end', resolve);
+    });
+
+    const transactions: Transaction[] = [];
+
+    csvTransactions.forEach(async csvTransaction => {
+      const transaction = await transactionRepository.execute(csvTransaction);
+      transactions.push(transaction);
+    });
+
+    return transactions;
   }
 }
 
